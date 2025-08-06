ac5e93bd71122ab2a2e73f22302931bc88e71103
Rodrigo Gedam <rogedam@gmail.com>
2025-08-06 11:14:30 -0300

    Mudanças About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -48,15 +48,6 @@ export default function About() {
 										</a>
 									</td>
 								</tr>
-								<tr>
-									<td className="font-headline font-bold uppercase text-black">Disponível</td>
-									<td>
-										<span className="relative flex h-3 w-3">
-											<span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
-											<span className="relative inline-flex h-3 w-3 rounded-full bg-green-600"></span>
-										</span>
-									</td>
-								</tr>
 							</tbody>
 						</table>
 					</div>
